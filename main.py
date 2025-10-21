f27aa5af843833b4eb392e49afe6138f24430fcf
ChetanKulthe <chetankulthe@IdeaPad-3-14ITL6-7ad10eca.localdomain>
2025-10-21 13:11:09 +0000

    added mongopassword

--- main.py
+++ main.py
@@ -11,5 +11,22 @@ async def app_init():
     client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://ckulthe56_db_user:N3RmF60r9xaBtrBU@cluster0.tfyywqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     await init_beanie(database=client.blog_db, document_models=[Post])
 
+# Root route
+@app.get("/")
+async def root():
+    return {"status": "FastAPI Secure Blog is running 🚀"}
+
 app.include_router(posts.router)
 app.include_router(admin.router)
+
+
+"""
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def home():
+    return {"status": "FastAPI blog is running 🚀"}
+"""
+
