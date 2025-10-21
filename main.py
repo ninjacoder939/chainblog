d3fbed65d3aa3e4a87d88b9ea9b9265c7c71e959
ChetanKulthe <chetankulthe@IdeaPad-3-14ITL6-7ad10eca.localdomain>
2025-10-21 13:21:19 +0000

    added index.html

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 from beanie import init_beanie
 import motor.motor_asyncio
 from routes import posts, admin
@@ -6,27 +7,22 @@ from models import Post
 
 app = FastAPI(title="Secure Blog API")
 
+templates = Jinja2Templates(directory="templates")
+
 @app.on_event("startup")
 async def app_init():
-    client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://ckulthe56_db_user:N3RmF60r9xaBtrBU@cluster0.tfyywqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        "mongodb+srv://ckulthe56_db_user:N3RmF60r9xaBtrBU@cluster0.tfyywqo.mongodb.net/blog_db?retryWrites=true&w=majority&tls=true"
+    )
     await init_beanie(database=client.blog_db, document_models=[Post])
 
-# Root route
+# Landing page
 @app.get("/")
-async def root():
-    return {"status": "FastAPI Secure Blog is running 🚀"}
+async def root(request: Request):
+    posts_list = await Post.find_all().to_list()
+    return templates.TemplateResponse("index.html", {"request": request, "posts": posts_list})
 
+# Include routers
 app.include_router(posts.router)
 app.include_router(admin.router)
-
-
-"""
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-async def home():
-    return {"status": "FastAPI blog is running 🚀"}
-"""
 
